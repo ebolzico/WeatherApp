85e821976d246d63782c9cba10ac19f247f917d1
Emanuel <bolzicoemanuel@gmail.com>
2021-04-06 15:32:30 -0300

    swal alert

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,15 +3,26 @@ import React from 'react';
 import SearchBar from './SearchBar.jsx';
 import './Nav.css';
 import {Link} from 'react-router-dom';
- 
+import swal from 'sweetalert'
 
 function Nav({onSearch}) { //onSearch= significa que le das un atributo con el valor {onSearch}
+  function about(){
+    swal({
+      title: 'App by Emanuel Bolzico',
+      text: 'This is a non-profit app, developed with the purpouse of learning new habilities',
+      buton:['Esc', 'Know more about me']
+    }).then(() => {
+      swal({
+        text: 'I am 26 year old man that wants to learn how to make efficient and comftarble websites with good visuals. For more of my works, feel free to visit my github!'
+      })
+    })
+  }
   return (
     <div className= "search">
       <Link exact to= '/'>
       <h2 className="name">Weather App</h2>
       </Link>
-      <span>About</span>
+      <button onClick= {about}>About</button>
       <SearchBar onSearch={onSearch}/>  
     </div>
   );
