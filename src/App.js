ab49bfa4752c86d0a0eef212e384531097a639fe
Emanuel <bolzicoemanuel@gmail.com>
2021-04-06 14:13:32 -0300

    env variable

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import Ciudad from './components/Ciudad.jsx';
 export default function App() {
   const [cities, setCities] = useState([]);
   function onSearch(ciudad) {
-    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${ciudad}&appid=${'4ae2636d8dfbdc3044bede63951a019b'}&units=metric`)
+    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${ciudad}&appid=${process.env.REACT_APP_API_KEY}&units=metric`)
       .then(r => r.json())
       .then((recurso) => {
         if(recurso.main !== undefined){
